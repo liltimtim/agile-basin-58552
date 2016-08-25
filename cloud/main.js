1ce87dca85b829093f79092032ca9b65aba71e4a
Timothy Barrett <liltimtim@gmail.com>
2016-08-24 20:52:55 -0400

    added mailgun adapter can send messages

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,28 @@
-
+var api_key = 'key-2ab74683afc2e01ab2814713eae4ffba';
+var domain = 'sandbox42c1e3c129af4a5993f997970ef60e2c.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+
+});
+
+Parse.Cloud.define('submitJobRequest', function(request, response){
+  var params = request.params;
+  if (params.email != null && params.firstName != null && params.lastName != null) {
+      var data = {
+      from: 'Test User <test@sample.mailgun.org>',
+      to: request.params.email,
+      subject: 'Hello Test Heroku',
+      text: 'Test message from your heroku server'
+    };
+    mailgun.messages().send(data, function(error, body){
+      console.log(body);
+      if (error == null) {
+        response.success("Sent Message");
+      } else {
+        response.error(error);
+      }
+    });
+  } else {
+    response.error("required parameters not sent");
+  }
 });
